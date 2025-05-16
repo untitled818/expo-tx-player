d3b6bf56b7ccaac75e1bb92f2c5e65dd22894994
sunyangy <2581613136@qq.com>
2025-05-16 17:52:39 +0800

    fix: 集成腾讯sdk，全屏布局导致程序崩溃

--- android/src/main/java/expo/modules/txplayer/ExpoTxPlayerModule.kt
+++ android/src/main/java/expo/modules/txplayer/ExpoTxPlayerModule.kt
@@ -3,8 +3,15 @@ package expo.modules.txplayer
 import expo.modules.kotlin.modules.Module
 import expo.modules.kotlin.modules.ModuleDefinition
 import java.net.URL
+import android.util.Log
+import com.tencent.rtmp.TXLiveBase
+import com.tencent.rtmp.TXLiveBaseListener
 
 class ExpoTxPlayerModule : Module() {
+  companion object {
+    private const val TAG = "ExpoTxPlayer"
+  }
+
   // Each module class must implement the definition function. The definition consists of components
   // that describes the module's functionality and behavior.
   // See https://docs.expo.dev/modules/module-api for more details about available components.
@@ -13,6 +20,21 @@ class ExpoTxPlayerModule : Module() {
     // Can be inferred from module's class name, but it's recommended to set it explicitly for clarity.
     // The module will be accessible from `requireNativeModule('ExpoTxPlayer')` in JavaScript.
     Name("ExpoTxPlayer")
+
+    // Initialize Tencent Live SDK
+    OnCreate {
+      val licenseURL = "https://license.vod2.myqcloud.com/license/v2/1258384072_1/v_cube.license" // 获取到的 license url
+      val licenseKey = "4c71bd88da95af202a8f3b2743c7e4e4" // 获取到的 license key
+      
+      TXLiveBase.getInstance().setLicence(appContext.reactContext, licenseURL, licenseKey)
+      Log.d("ExpoTxPlayerModuless", "setLicence: $licenseURL, $licenseKey")
+      TXLiveBase.setListener(object : TXLiveBaseListener() {
+       override fun onLicenceLoaded(result: Int, reason: String?) {
+         Log.i(TAG, "onLicenceLoaded: result:$result, reason:$reason")
+       }
+     })
+    }
+
 
     // Sets constant properties on the module. Can take a dictionary or a closure that returns a dictionary.
     Constants(
@@ -42,7 +64,10 @@ class ExpoTxPlayerModule : Module() {
       // Defines a setter for the `url` prop.
       Prop("url") { view: ExpoTxPlayerView, url: URL ->
         view.webView.loadUrl(url.toString())
+        // Log.d("ExpoTxPlayerModule", "Calling setVideoUrl with: $url")
+        // view.setVideoUrl(url.toString())
       }
+      // Log.d("ExpoTxPlayerModule触发了吗")
       // Defines an event that the view can send to JavaScript.
       Events("onLoad")
     }
--- android/src/main/java/expo/modules/txplayer/ExpoTxPlayerView.kt
+++ android/src/main/java/expo/modules/txplayer/ExpoTxPlayerView.kt
@@ -6,7 +6,11 @@ import android.webkit.WebViewClient
 import expo.modules.kotlin.AppContext
 import expo.modules.kotlin.viewevent.EventDispatcher
 import expo.modules.kotlin.views.ExpoView
-
+import com.tencent.liteav.demo.superplayer.SuperPlayerView
+import com.tencent.liteav.demo.superplayer.SuperPlayerModel
+import android.util.Log
+import android.view.ViewGroup
+import android.util.TypedValue
 class ExpoTxPlayerView(context: Context, appContext: AppContext) : ExpoView(context, appContext) {
   // Creates and initializes an event dispatcher for the `onLoad` event.
   // The name of the event is inferred from the value and needs to match the event name defined in the module.
@@ -23,8 +27,59 @@ class ExpoTxPlayerView(context: Context, appContext: AppContext) : ExpoView(cont
     }
   }
 
+  // 创建 SuperPlayerView 实例
+  // private val playerView = SuperPlayerView(context).apply {
+  //   layoutParams = LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT)
+  // }
+
+  // fun setVideoUrl(url: String) {
+  //   val model = SuperPlayerModel().apply {
+  //     appId = 1308280968 // 替换为你的实际 AppId
+  //     this.url = url
+  //   }
+  //   playerView.playWithModelNeedLicence(model)
+  // }
+
   init {
-    // Adds the WebView to the view hierarchy.
-    addView(webView)
+    try {
+    Log.d("ExpoTxPlayerView", "Initializing playerView...")
+    val playerView = SuperPlayerView(context).apply {
+      layoutParams = ViewGroup.LayoutParams(
+        ViewGroup.LayoutParams.MATCH_PARENT,  // width
+        TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP, 
+            200f, 
+            resources.displayMetrics
+        ).toInt() // height=200dp
+      )
+    }
+    // addView(webView)
+    addView(playerView)
+  } catch (e: Exception) {
+    Log.e("ExpoTxPlayerView", "Error creating SupessrPlayer6View: ${e.message}", e)
+    throw e
+    }
   }
+  // private val playerView: SuperPlayerView
+  // init {
+  //   // 1. 创建基础视图容器
+  //   playerView = SuperPlayerView(context).apply {
+  //       layoutParams = ViewGroup.LayoutParams(
+  //           ViewGroup.LayoutParams.MATCH_PARENT,
+  //           ViewGroup.LayoutParams.MATCH_PARENT
+  //       )
+  //   }
+    
+  //   // 2. 反射跳过内部布局加载
+  //   try {
+  //       val clazz = Class.forName("com.tencent.liteav.demo.superplayer.SuperPlayerView")
+  //       val field = clazz.getDeclaredField("mLayoutInflateSuccess")
+  //       field.isAccessible = true
+  //       field.setBoolean(playerView, true)
+  //   } catch (e: Exception) {
+  //       Log.e("Player", "跳过布局加载失败", e)
+  //   }
+    
+  //   addView(playerView)
+  // }
 }
