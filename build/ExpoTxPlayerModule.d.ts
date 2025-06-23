0bec214e3051da627c1d60737a726aa66987356e
wangbingII <453325924@qq.com>
2025-06-23 10:56:28 +0800

    feat: add build

---
+++ build/ExpoTxPlayerModule.d.ts
@@ -0,0 +1,7 @@
+import { NativeModule } from "expo";
+import { ExpoTxPlayerModuleEvents } from "./ExpoTxPlayer.types";
+declare class ExpoTxPlayerModule extends NativeModule<ExpoTxPlayerModuleEvents> {
+}
+declare const _default: ExpoTxPlayerModule;
+export default _default;
+//# sourceMappingURL=ExpoTxPlayerModule.d.ts.map
---
+++ build/index.d.ts
@@ -0,0 +1,4 @@
+export { default } from "./ExpoTxPlayerModule";
+export { default as ExpoTxPlayerView } from "./ExpoTxPlayerView";
+export * from "./ExpoTxPlayer.types";
+//# sourceMappingURL=index.d.ts.map
